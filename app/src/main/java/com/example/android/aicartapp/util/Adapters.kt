08f579a91e357b2597e8c2ff4d6c0d8aebda944e
salgut408 <sgutie@saic.edu>
2022-06-06 16:04:18 -0500

    display image detail data presistance completed

--- app/src/main/java/com/example/android/aicartapp/util/Adapters.kt
+++ app/src/main/java/com/example/android/aicartapp/util/Adapters.kt
@@ -4,13 +4,9 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
-class Adapters {
-    companion object {
-        @BindingAdapter("pictureUrl")
-        fun bindUriToImage(imageView: ImageView, strUrl: String) {
-            Glide.with(imageView.context)
-                .load(strUrl)
-                .into(imageView)
-        }
-    }
+@BindingAdapter("pictureUrl")
+fun bindUriToImage(imageView: ImageView, strUrl: String) {
+    Glide.with(imageView.context)
+        .load(strUrl)
+        .into(imageView)
 }
