4368ce2443c8de5c48ec3d1f0380ea4cf1f69ff9
salgut408 <sgutie@saic.edu>
2022-10-10 13:53:00 -0500

    Material app theme bar added

--- app/src/main/java/com/salgut/android/aicartapp/MainActivity.kt
+++ app/src/main/java/com/salgut/android/aicartapp/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity() {
             )
         )
 //        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
+        navView.setupWithNavController(navController)
         setSupportActionBar(binding.myToolbar)
 
 
