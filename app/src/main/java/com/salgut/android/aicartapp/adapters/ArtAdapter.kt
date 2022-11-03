6e3353dc447d676c05a6205a15129d1c12f66591
salgut408 <sgutie@saic.edu>
2022-11-03 14:22:00 -0500

    No KotlinSynthetics used anymore

--- app/src/main/java/com/salgut/android/aicartapp/adapters/ArtAdapter.kt
+++ app/src/main/java/com/salgut/android/aicartapp/adapters/ArtAdapter.kt
@@ -1,35 +1,23 @@
 package com.salgut.android.aicartapp.adapters
 
 import android.graphics.Bitmap
-import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.core.graphics.drawable.toBitmap
 import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.RequestBuilder
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
-import com.salgut.android.aicartapp.R
 import com.salgut.android.aicartapp.databinding.ItemPrevNewBinding
 import com.salgut.android.aicartapp.models.ArtworkObject
-import kotlinx.android.synthetic.main.fragment_artwork_detail.view.*
-import kotlinx.android.synthetic.main.item_prev_new.view.*
-import kotlinx.android.synthetic.main.item_prev_new.view.artistNameDisplay
+
 
 class ArtAdapter : RecyclerView.Adapter<ArtAdapter.ArtworkObjectViewHolder>() {
-inner class ArtworkObjectViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    inner class ArtworkObjectViewHolder(val binding: ItemPrevNewBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     var dominantColor: Int = 0
 
@@ -45,13 +33,11 @@ inner class ArtworkObjectViewHolder(itemView: View): RecyclerView.ViewHolder(ite
     val differ = AsyncListDiffer(this, differCallback)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtworkObjectViewHolder {
-        return ArtworkObjectViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.item_prev_new,
-                parent,
-                false
-            )
-        )
+        return ArtworkObjectViewHolder(ItemPrevNewBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        ))
     }
 
     override fun getItemCount(): Int {
@@ -60,31 +46,24 @@ inner class ArtworkObjectViewHolder(itemView: View): RecyclerView.ViewHolder(ite
 
     override fun onBindViewHolder(holder: ArtworkObjectViewHolder, position: Int) {
         val artwork = differ.currentList[position]
-        holder.itemView.apply {
+        holder.binding.apply {
+
+            artistTitle.text = artwork.artistDisplay
+            artistNameDisplay.text = artwork.title
 
             var url = artwork.getOtherImgUrl()
 
-            Glide.with(this)
-                .asBitmap()
+            Glide.with(imageView.context)
 
 
                 .load(url)
 
 
-
-
                 .into(imageView)
 
-            artistTitle.text=artwork.artistDisplay
-            artistNameDisplay.text=artwork.title
 
 
-
-
-
-
-
-            setOnClickListener {
+            cardItem.setOnClickListener {
                 onItemClickListener?.let { it(artwork) }
             }
 
@@ -92,17 +71,16 @@ inner class ArtworkObjectViewHolder(itemView: View): RecyclerView.ViewHolder(ite
         }
     }
 
-    private var onItemClickListener:((ArtworkObject) -> Unit)? = null
+    private var onItemClickListener: ((ArtworkObject) -> Unit)? = null
 
     fun setOnItemClickListener(listener: (ArtworkObject) -> Unit) {
         onItemClickListener = listener
     }
 
 
-
     fun getDomColor(drawable: Drawable, binding: TextView) {
         val bmp = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
-        Palette.from(bmp).generate {palette ->
+        Palette.from(bmp).generate { palette ->
 
             palette?.dominantSwatch?.rgb?.toInt()
                 ?.let { binding.setTextColor(it) }
--- app/src/main/java/com/salgut/android/aicartapp/ui/artworkDetail/ArtworkDetailFragment.kt
+++ app/src/main/java/com/salgut/android/aicartapp/ui/artworkDetail/ArtworkDetailFragment.kt
@@ -1,21 +1,16 @@
 package com.salgut.android.aicartapp.ui.artworkDetail
 
 import android.content.res.ColorStateList
-import android.graphics.Bitmap
 import android.graphics.Color
-import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
 import androidx.core.graphics.drawable.toBitmap
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -23,12 +18,10 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
-import com.salgut.android.aicartapp.ui.MainArtViewModel
 import com.google.android.material.snackbar.Snackbar
 import com.salgut.android.aicartapp.databinding.FragmentArtworkDetailBinding
 import com.salgut.android.aicartapp.models.ArtworkObject
-import kotlinx.android.synthetic.main.item_prev_new.view.*
-
+import com.salgut.android.aicartapp.ui.MainArtViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.io.IOException
 
