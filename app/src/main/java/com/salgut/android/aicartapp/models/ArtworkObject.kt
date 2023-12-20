627764050199174038a55579e7a0cc49c8ae3f74
salgut408 <sgutie@saic.edu>
2023-12-20 12:16:14 -0600

    half done with all updates

--- app/src/main/java/com/salgut/android/aicartapp/models/ArtworkObject.kt
+++ app/src/main/java/com/salgut/android/aicartapp/models/ArtworkObject.kt
@@ -8,39 +8,36 @@ import kotlinx.android.parcel.Parcelize
 
 
 @Entity(
-  tableName = "artworks"
+    tableName = "artworks"
 
 )
 @Parcelize
- data class ArtworkObject (
+data class ArtworkObject(
 
-  @SerializedName("_score"               ) val score               : Double?  = null,
-  @SerializedName("is_public_domain"     ) val isPublicDomain      : Boolean? = null,
-  @SerializedName("medium_display"       ) val mediumDisplay       : String?  = null,
-  @SerializedName("colorfulness"         ) val colorfulness        : Double?  = null,
-  @SerializedName("inscriptions"         ) val inscriptions        : String?  = null,
-  @SerializedName("latitude"             ) val latitude            : String?  = null,
-  @SerializedName("artist_display"       ) val artistDisplay       : String?  = null,
-  @SerializedName("artist_title"         ) val artistTitle         : String?  = null,
-  @SerializedName("provenance_text"      ) val provenanceText      : String?  = null,
-  @SerializedName("title"                ) val title               : String?  = null,
-  @SerializedName("place_of_origin"      ) val placeOfOrigin       : String?  = null,
-  @SerializedName("credit_line"          ) val creditLine          : String?  = null,
-  @PrimaryKey(autoGenerate = false)
-  @SerializedName("id"                   ) val id                  : Int?     = null,
-  @SerializedName("image_id"             ) val imageId             : String?  = null,
-  @SerializedName("classification_title" ) val classificationTitle : String?  = null,
-  @SerializedName("style_title"          ) val styleTitle          : String? = null,
-  @SerializedName("longitude"            ) val longitude           : String?  = null
-): Parcelable
-
-
-{
+    @SerializedName("_score") val score: Double? = null,
+    @SerializedName("is_public_domain") val isPublicDomain: Boolean? = null,
+    @SerializedName("medium_display") val mediumDisplay: String? = null,
+    @SerializedName("colorfulness") val colorfulness: Double? = null,
+    @SerializedName("inscriptions") val inscriptions: String? = null,
+    @SerializedName("latitude") val latitude: String? = null,
+    @SerializedName("artist_display") val artistDisplay: String? = null,
+    @SerializedName("artist_title") val artistTitle: String? = null,
+    @SerializedName("provenance_text") val provenanceText: String? = null,
+    @SerializedName("title") val title: String? = null,
+    @SerializedName("place_of_origin") val placeOfOrigin: String? = null,
+    @SerializedName("credit_line") val creditLine: String? = null,
+    @PrimaryKey(autoGenerate = false)
+    @SerializedName("id") val id: Int? = null,
+    @SerializedName("image_id") val imageId: String? = null,
+    @SerializedName("classification_title") val classificationTitle: String? = null,
+    @SerializedName("style_title") val styleTitle: String? = null,
+    @SerializedName("longitude") val longitude: String? = null,
+) : Parcelable {
 
 
-  fun getOtherImgUrl(): String {
-    val urlForDisplay = "https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg"
-    return urlForDisplay
-  }
+    fun getOtherImgUrl(): String {
+        val urlForDisplay = "https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg"
+        return urlForDisplay
+    }
 
 }
